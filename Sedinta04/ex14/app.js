ebf9bcf94206b395ebc770e1ea8cb633fdac84c6
iuliaaaaaa <48695047+iuliaaaaaa@users.noreply.github.com>
2019-03-28 11:13:05 +0200

    tema

--- Sedinta04/ex14/app.js
+++ Sedinta04/ex14/app.js
@@ -1,7 +1,7 @@
 var person = {
   firstName: 'Dragos',
   lastName: 'Iordache',
-  birthYear: 1987, 
+  birthYear: 1987,
   pets: [{
     name: 'Twix',
     species: 'papagal',
@@ -9,7 +9,7 @@ var person = {
   }, {
     name: 'Mars',
     species: 'caine',
-    age: 2, 
+    age: 2,
   }, {
     name: 'Bounty',
     species: 'hamster',
@@ -20,3 +20,29 @@ var person = {
     age: 10
   }]
 };
+
+var pets = ['Twix','Mars','Lion'];
+pets.forEach(function(element, index, array) {
+  console.log(element);
+});
+
+var totalSum = 0;
+for(i = 0; i < person.pets.length ; i++) {
+  var age = person.pets[i].age;
+  totalSum = totalSum + age
+console.log(totalSum);
+};
+
+for(i = 0; i <person.pets.length ; i++) {
+function logArrayElements(element, index, array) {
+  console.log(element + ' este ' + person.pets[i].species + ' si are ' +
+  person.pets[i].age + ' ani ');
+}
+pets.forEach(logArrayElements);
+};
+var difference = 0
+for(i = 0; i < person.pets.length; i++) {
+  difference = person.age - person.pets[i].age;
+  console.log(' Intre ' + person.firstName + ' si ' + person.pets[i].name +
+  ' este o diferenta de ' + difference + ' ani ');
+}
